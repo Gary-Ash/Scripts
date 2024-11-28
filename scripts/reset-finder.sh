b5291b15df38f5f80f2f9f9f58540cedb1a57de0
Gary Ash <gary.ash@icloud.com>
2024-11-28 14:39:07 -0500

    Initial commit

---
+++ scripts/reset-finder.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+#*****************************************************************************************
+# reset-finder.sh
+#
+# reset Finder settings
+#
+# Author   :  Gary Ash <gary.ash@icloud.com>
+# Created  :  28-Nov-2024  1:33pm
+# Modified :
+#
+# Copyright © 2024 By Gary Ash All rights reserved.
+#*****************************************************************************************
+reset-finder() {
+	defaults delete com.apple.finder
+
+	defaults write com.apple.finder NewWindowTarget -string "PfHm"
+	defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
+	defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+	defaults write com.apple.finder ShowStatusBar -bool true
+	defaults write com.apple.finder ShowRecentTags -bool false
+
+	defaults write com.apple.finder _FXSortFoldersFirst -bool true
+	defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+	defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+	killall Finder
+}
---
+++ scripts/update-site.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+#*****************************************************************************************
+# update-site.sh
+#
+# This script will update my Gee Dbl A website/blog
+#
+# Author   :  Gary Ash <gary.ash@icloud.com>
+# Created  :  28-Nov-2024  1:33pm
+# Modified :
+#
+# Copyright © 2024 By Gary Ash All rights reserved.
+#*****************************************************************************************
+
+read -s "?Password: " password
+echo
+
+if [[ -n $password ]]; then
+	cd ~/Sites/geedbla.com || return
+
+	if jekyll build >/dev/null; then
+		sshpass -p "$password" rsync -arz --exclude=".gitkeep" "$HOME/Sites/geedbla.com/_site/" "$USER@geedbla.com:~/geedbla.com"
+		rm -rf "$HOME/Sites/geedbla.com/_site"
+		rm -rf "$HOME/Sites/geedbla.com/.jekyll-cache"
+		rm -rf "$HOME/Sites/geedbla.com/.jekyll-metadata"
+	else
+		echo "Error building the site"
+	fi
+fi

aeaee45bf4c3143e552ef2386ee0afbc03c2ca39
Gary Ash <gary.ash@icloud.com>
2025-05-09 20:36:53 -0400

    [Feature] Better Process Control Do a better job controlling of processes I'm trying to clean up after

--- scripts/update-dots.sh
+++ scripts/update-dots.sh
@@ -5,7 +5,7 @@
 # This script automates the maintenance of my dot files repository
 #
 # Author   :  Gary Ash <gary.ash@icloud.com>
-# Created  :   1-May-2025  3:22pm
+# Created  :  17-May-2025  9:57pm
 # Modified :
 #
 # Copyright © 2024-2025 By Gary Ash All rights reserved.
